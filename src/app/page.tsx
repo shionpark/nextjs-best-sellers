84bfca58e8b3906835722096a50bc2a566be68ba
bkkom <seoy1108@gmail.com>
2023-10-15 15:03:13 +0900

    Feat: ✨ Fetching Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import { getHardCover, getLists } from "@/utils";
+
+export default async function Home() {
+  const data = await getLists();
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        {data.results.map((list, idx) => (
+          <li style={{ border: "1px solid white", margin: "10px" }} key={idx}>
+            {list.list_name} &rarr;
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
+import { IBook } from "@/types";
+
 export const BASE_URL = "https://books-api.nomadcoders.workers.dev";
 
-export const GET_LIST = `${BASE_URL}/list`;
+export function getLists(): Promise<{ results: IBook[] }> {
+  return fetch(`${BASE_URL}/lists`).then((r) => r.json());
+}
 
-export const GET_HARD_COVER = `${GET_LIST}?name=hardcover-fiction`;
+export function getHardCover() {
+  return fetch(`${BASE_URL}/lists?name=hardcover-fiction`).then((r) =>
+    r.json()
+  );
+}
